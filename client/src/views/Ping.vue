65ef0557a172fa32f6a2083edf0c04ad90fdd6be
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-25 23:49:28 +0900

    feat: add ping API

--- client/src/views/Ping.vue
+++ client/src/views/Ping.vue
@@ -1,16 +1,42 @@
 <template>
-	<div class="ping">
-		<h1>{{ msg }}</h1>
-	</div>
+  <div class="ping">
+    <h1>{{ msg }}</h1>
+    <p>test</p>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-	name: 'PingPage',
-	data() {
-		return {
-			msg: '',
-		};
-	},
+  name: "PingPage",
+
+  data() {
+    return {
+      msg: "",
+    };
+  },
+
+  methods: {
+    getMessage() {
+      const path = "http://localhost:5000/ping";
+
+      axios
+        .get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+
+  // 컴포넌트가 생성되는 시점
+  // (아직 virtual DOM 반영 X)
+  created() {
+    console.log("hello");
+    this.getMessage();
+  },
 };
 </script>
