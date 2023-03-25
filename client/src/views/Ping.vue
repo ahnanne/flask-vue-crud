63e422f61f5fab3c2720970e4b59723fd94b77e9
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-25 23:22:18 +0900

    style: auto format

--- client/src/views/Ping.vue
+++ client/src/views/Ping.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="ping">
-    <h1>{{ msg }}</h1>
-  </div>
+	<div class="ping">
+		<h1>{{ msg }}</h1>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Ping',
-  data() {
-    return {
-      msg: "Pong!",
-    };
-  }
+	name: 'PingPage',
+	data() {
+		return {
+			msg: '',
+		};
+	},
 };
 </script>
