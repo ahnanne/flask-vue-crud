e9444ddfa0c58f11758a3c6d3280b2a91da0b052
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-25 22:40:55 +0900

    feat: Ping 페이지 추가

---
+++ client/src/views/Ping.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ping">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: "Pong!",
+    };
+  }
+};
+</script>
