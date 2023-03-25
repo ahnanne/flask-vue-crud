8dfc6e1ea7820dd9ab19b9bb893d54a9afafac1a
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-25 23:47:21 +0900

    chore: add prettier rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,37 +1,22 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:vue/essential',
-		'prettier',
-		'plugin:prettier/recommended',
-		'@vue/prettier',
-	],
-	plugins: ['prettier'],
-	parserOptions: {
-		parser: '@babel/eslint-parser',
-		requireConfigFile: false,
-		babelOptions: {
-			babelrc: false,
-			configFile: false,
-		},
-	},
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{
-				singleQuote: true,
-				semi: true,
-				useTabs: true,
-				tabWidth: 2,
-				trailingComma: 'all',
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: 'avoid',
-			},
-		],
-	},
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier",
+    "plugin:prettier/recommended",
+    "@vue/prettier",
+  ],
+  plugins: ["prettier"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+    },
+  },
 };
