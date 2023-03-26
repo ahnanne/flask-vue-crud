fb5c3c09e4ad11aa2686a42c7b0e94dd4d2192a4
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-26 16:06:51 +0900

    chore: 프로덕션 모드에서 미사용 클래스 제거하는 purgecss 플러그인 추가

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,19 @@
+const postcssPurgecss = require("@fullhuman/postcss-purgecss");
+
+const purgecss = postcssPurgecss({
+  content: ["./public/**/*.html", "./src/**/*.vue", "./src/**/*.js"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).-move$/,
+    /^router-link(|-exact)-active$/,
+  ],
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
     require("autoprefixer")(),
+    process.env.NODE_ENV !== "development" ? purgecss : "",
   ],
 };
