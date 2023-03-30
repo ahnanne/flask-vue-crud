061fd646640b1321247f5e3deb8086d544f5a6bb
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-30 16:16:45 +0900

    test: 기본 CRUD 실습

---
+++ server/crud.py
@@ -0,0 +1,48 @@
+from app import app
+from init_db import db, Puppy
+
+app_ctx = app.app_context()
+app_ctx.push()
+
+
+## CREATE ##
+my_puppy = Puppy('Rufus', 5)
+db.session.add(my_puppy)
+db.session.commit()
+
+
+## READ ##
+all_puppies = Puppy.query.all() # 테이블에 있는 모든 강아지 객체들
+print(all_puppies)
+
+# FILTERS
+puppy2 = Puppy.query.filter_by(name='Frankie').all()
+
+# SELECT BY ID
+puppy1 = Puppy.query.get(1)
+if puppy1 == None:
+    puppy1 = puppy2[0]
+else:
+    print(puppy1.name)
+
+
+## UPDATE ##
+puppy1.age = 100
+db.session.add(puppy1)
+db.session.commit()
+
+
+## DELETE ##
+puppy3 = Puppy.query.get(2)
+print(puppy3) # 존재하지 않을 경우 None
+if puppy3 != None:
+    db.session.delete(puppy3)
+    db.session.commit()
+else:
+    print('삭제할 대상이 존재하지 않습니다.')
+
+all_puppies = Puppy.query.all()
+print(all_puppies)
+
+
+app_ctx.pop()
